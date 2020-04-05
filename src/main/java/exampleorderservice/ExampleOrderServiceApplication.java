8f97358c0f3483d33e0804ed25d980d352f69362
Junfeng Dai <ryandjf@hotmail.com>
2020-04-05 09:29:49 +0800

    Initial import

---
+++ src/main/java/exampleorderservice/ExampleOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package exampleorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleOrderServiceApplication.class, args);
+	}
+
+}
