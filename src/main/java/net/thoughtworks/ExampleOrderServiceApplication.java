81f9d198f1e73d99dc3989b48e79878f6838aa77
Junfeng Dai <ryandjf@hotmail.com>
2020-04-07 14:49:10 +0800

    add more example code

--- src/main/java/net/thoughtworks/ExampleOrderServiceApplication.java
+++ src/main/java/net/thoughtworks/ExampleOrderServiceApplication.java
@@ -13,6 +13,7 @@ public class ExampleOrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleOrderServiceApplication.class, args);
     }
+    
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
