1c4ffcc647f9c99e5a5a4ccfc3b969ed4c2917cc
Junfeng Dai <ryandjf@hotmail.com>
2020-04-07 14:48:27 +0800

    add more example code

--- src/main/java/net/thoughtworks/ExampleOrderServiceApplication.java
+++ src/main/java/net/thoughtworks/ExampleOrderServiceApplication.java
@@ -3,6 +3,9 @@ package net.thoughtworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ExampleOrderServiceApplication {
@@ -10,5 +13,8 @@ public class ExampleOrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExampleOrderServiceApplication.class, args);
     }
-
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
 }
