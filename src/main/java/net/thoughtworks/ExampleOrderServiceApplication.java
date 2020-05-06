ee83f79c37824836124737f630f514c45bc91b33
Junfeng Dai <ryandjf@hotmail.com>
2020-05-06 14:05:44 +0800

    upgrade to jdk 11

--- src/main/java/net/thoughtworks/ExampleOrderServiceApplication.java
+++ src/main/java/net/thoughtworks/ExampleOrderServiceApplication.java
@@ -3,18 +3,11 @@ package net.thoughtworks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class ExampleOrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExampleOrderServiceApplication.class, args);
-    }
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
     }
 }
